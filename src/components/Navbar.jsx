3643cb9f1956ec61f5345fcd2f34a14d87169d29
AshishDentread <ashish@dentread.com>
2024-06-18 15:25:15 +0530

    added mail link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -133,7 +133,7 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
 						<a
 							className="flex justify-between items-center w-full text-gray-600"
-							href="ashish0817tomer@gmail.com"
+							href="https://ashish0817tomer@gmail.com"
 						>
 							Mail <HiOutlineMail size={30} />
 						</a>
