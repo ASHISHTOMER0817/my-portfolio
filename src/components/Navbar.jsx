8f37fd603b0273ba8e0f8427f1b15ed35f146270
AshishDentread <ashish@dentread.com>
2024-06-18 14:44:43 +0530

    updated the portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
 import logo from "../assets/images/logo1.png";
-import {
-	FaBars,
-	FaTimes,
-	FaGithub,
-	FaLinkedin,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import {Link} from "react-scroll"
+import { Link } from "react-scroll";
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
@@ -16,37 +11,45 @@ const Navbar = () => {
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 			<div>
-				<img
-					src={logo}
-					alt="Logo"
-					style={{ width: "50px" }}
-				/>
+				<img src={logo} alt="Logo" style={{ width: "50px" }} />
 			</div>
 
 			{/* menu */}
 
 			<ul className="hidden md:flex">
 				<li>
-					<Link to="home" smooth={true}duration={500}>Home</Link>
+					<Link to="home" smooth={true} duration={500}>
+						Home
+					</Link>
 				</li>
 				<li>
-					<Link to="about" smooth={true}duration={500}>About</Link>
+					<Link to="about" smooth={true} duration={500}>
+						About
+					</Link>
 				</li>
 				<li>
-					<Link to="skills" smooth={true}duration={500}>Skills</Link>
+					<Link to="skills" smooth={true} duration={500}>
+						Skills
+					</Link>
 				</li>
 				<li>
-					<Link to="work" smooth={true}duration={500}>Work</Link>
+					<Link to="work" smooth={true} duration={500}>
+						Work
+					</Link>
 				</li>
 				<li>
-					<Link to="contact" smooth={true}duration={500}>Contact</Link>
+					<Link to="contact" smooth={true} duration={500}>
+						Contact
+					</Link>
 				</li>
-				
 			</ul>
 
 			{/* Hamburger */}
-			<div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
-				{!nav ? <FaBars /> : <FaTimes  />}
+			<div
+				onClick={handleClick}
+				className="md:hidden z-10 cursor-pointer"
+			>
+				{!nav ? <FaBars /> : <FaTimes />}
 			</div>
 			{/* Mobile Menu */}
 
@@ -58,19 +61,54 @@ const Navbar = () => {
 				}
 			>
 				<li className="py-6 text-4xl">
-					<Link onClick={handleClick} to="home" smooth={true}duration={500}>Home</Link>
+					<Link
+						onClick={handleClick}
+						to="home"
+						smooth={true}
+						duration={500}
+					>
+						Home
+					</Link>
 				</li>
 				<li className="py-6 text-4xl">
-					<Link onClick={handleClick} to="about" smooth={true}duration={500}>About</Link>
+					<Link
+						onClick={handleClick}
+						to="about"
+						smooth={true}
+						duration={500}
+					>
+						About
+					</Link>
 				</li>
 				<li className="py-6 text-4xl">
-					<Link onClick={handleClick} to="skills" smooth={true}duration={500}>Skills</Link>
+					<Link
+						onClick={handleClick}
+						to="skills"
+						smooth={true}
+						duration={500}
+					>
+						Skills
+					</Link>
 				</li>
 				<li className="py-6 text-4xl">
-					<Link onClick={handleClick} to="work" smooth={true}duration={500}>Work</Link>
+					<Link
+						onClick={handleClick}
+						to="work"
+						smooth={true}
+						duration={500}
+					>
+						Work
+					</Link>
 				</li>
 				<li className="py-6 text-4xl">
-					<Link onClick={handleClick} to="contact" smooth={true}duration={500}>Contact</Link>
+					<Link
+						onClick={handleClick}
+						to="contact"
+						smooth={true}
+						duration={500}
+					>
+						Contact
+					</Link>
 				</li>
 			</ul>
 			{/* Social Icons */}
@@ -79,7 +117,7 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
 						<a
 							className="flex justify-between items-center w-full text-gray-300"
-							href="/"
+							href="https://www.linkedin.com/in/ashish-tomer-08172004-thatsme/"
 						>
 							Linkedin <FaLinkedin size={30} />
 						</a>
@@ -87,17 +125,17 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
 						<a
 							className="flex justify-between items-center w-full text-gray-300"
-							href="/"
+							href="https://github.com/ASHISHTOMER0817"
 						>
-							Linkedin <FaGithub size={30} />
+							Github <FaGithub size={30} />
 						</a>
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
 						<a
-							className="flex justify-between items-center w-full text-gray-300"
-							href="/"
+							className="flex justify-between items-center w-full text-gray-600"
+							href="ashish0817tomer@gmail.com"
 						>
-							Linkedin <HiOutlineMail size={30} />
+							Mail <HiOutlineMail size={30} />
 						</a>
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
@@ -105,7 +143,8 @@ const Navbar = () => {
 							className="flex justify-between items-center w-full text-gray-300"
 							href="/"
 						>
-							Linkedin <BsFillPersonLinesFill size={30} />
+							Linkedin{" "}
+							<BsFillPersonLinesFill size={30} />
 						</a>
 					</li>
 				</ul>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,62 +1,67 @@
 import React from "react";
-import workImg from "../assets/images/workImg.jpeg";
-import realEstate from "../assets/images/realestate.jpg";
+import cardmitra from "../assets/images/cardmitra.png";
+import chiton from "../assets/images/chiton.png";
 
 const Work = () => {
 	return (
-		<div name="work" className="w-full h-screen text-gray-300 bg-[#0a192f]">
+		<div
+			name="work"
+			className="w-full h-screen text-gray-300 bg-[#0a192f]"
+		>
 			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 				<div className="pb-8">
 					<p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
 						Work
 					</p>
-					<p className="py-6">
-					</p>
+					<p className="py-6"></p>
 				</div>
-				<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+				<div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
 					<div
 						style={{
-							backgroundImage: `url(${workImg})`,
+							backgroundImage: `url(${cardmitra})`,
 						}}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+								CardMitra
 							</span>
 							<div className="">
-								<a href="/">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-										Demo
-									</button>
+								<a
+									href="https://cardmitra.netlify.app"
+									className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+								>
+									Demo
 								</a>
-								<a href="/">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-										Code
-									</button>
+
+								<a
+									href="https://github.com/ASHISHTOMER0817/CardMitra"
+									className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
+								>
+									Code
 								</a>
 							</div>
 						</div>
 					</div>
 					<div
 						style={{
-							backgroundImage: `url(${realEstate})`,
+							backgroundImage: `url(${chiton})`,
 						}}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								React JS Application
+								Chiton App
 							</span>
 							<div className="">
-								<a href="/">
+								<a href="https://chiton.netlify.app/">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Demo
 									</button>
 								</a>
-								<a href="/">
+								<a href="https://github.com/ASHISHTOMER0817/chiton-1.0">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
 										Code
 									</button>
